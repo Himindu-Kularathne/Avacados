535eedad38d445dcdedb1248cd0adc2cab7dfa7b
Himindu <himindukularathne@gmail.com>
2024-02-08 09:01:00 +0530

    Initial Commit

---
+++ Avacados/AvacadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvacadosApp.swift
+//  Avacados
+//
+//  Created by Himindu Kularathne on 2024-02-08.
+//
+
+import SwiftUI
+
+@main
+struct AvacadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
